8d784a771dfe527467b9bf89c66706a332c0875c
Christopher Elwell <elwell.christopher@gmail.com>
2016-02-12 14:42:06 -0800

    blank lein library

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to opt
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/opt/core_test.clj
@@ -0,0 +1,7 @@
+(ns opt.core-test
+  (:require [clojure.test :refer :all]
+            [opt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
