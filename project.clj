8b00fab98fe678c70f37789a3b9a3eb6e3ed51ff
Christopher Elwell <elwell.christopher@gmail.com>
2016-02-12 16:00:39 -0800

    java json classes dep

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject opt "0.1.0-SNAPSHOT"
   :description "Purple optimization library."
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.twilio.sdk/twilio-java-sdk "4.2.0"] ;; kludge to pull in json java classes
+                 ]
   :java-source-paths ["src/java"])
