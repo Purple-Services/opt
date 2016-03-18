d08ac250b97d4ac142ecbe97c6237bc4d93bda59
Xi Han <xihan94@ucla.edu>
2016-03-18 13:31:07 -0700

    Update interface of gas recommendation.

--- src/opt/gas_station_recommendation.clj
+++ src/opt/gas_station_recommendation.clj
@@ -6,13 +6,19 @@
   []
   (helpers/update-local-stations-file))
 
+;; get rid of all stations that don't have 91.
+;; add a opt map to handle options.
 (defn compute-suggestions
-  "Take the lat/lng of source and destination and returns a list of
-   suggested stations and their total driving time.
+  "Take the lat/lng of source and destination (optional) and returns a list of
+   suggested stations and their total driving time. The output will be sorted
+   by their driving time in descending order.
 
-   Sample output: [{:station {...} :total-driving-time 4242},
-                   {:station {...} :total-driving-time 2424}]"
-  [src-lat src-lng dst-lat dst-lng]
-  (helpers/suggest-gas-stations-with-driving-time src-lng src-lat dst-lng dst-lat))
+   Sample output: [{:station {...} :total-driving-time 2424},
+                   {:station {...} :total-driving-time 4242}]"
+  ([src-lat src-lng dst-lat dst-lng opt]
+    (helpers/suggest-gas-stations-with-driving-time src-lng src-lat dst-lng dst-lat opt))
+  ([src-lat src-lng opt]
+    (helpers/suggest-gas-stations-near src-lng src-lat opt)))
+
 
 
