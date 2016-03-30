96c2752cf083c4c3ccc21cbee45685be286e3585
Christopher Elwell <elwell.christopher@gmail.com>
2016-03-29 18:23:09 -0700

    v1.0.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject opt "0.1.0-SNAPSHOT"
+(defproject opt "1.0.0-SNAPSHOT"
   :description "Purple optimization library."
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.1.0"]
                  ;; kludge to pull in json java classes
                  ;; can be removed after we port purpleOpt over to clojure
                  [com.twilio.sdk/twilio-java-sdk "4.2.0"]
-                 [common "1.0.0-SNAPSHOT"]
+                 [common "1.0.2-SNAPSHOT"]
                  [org.clojure/data.json "0.2.6"]]
   :java-source-paths ["src/java"])
