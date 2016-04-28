0213d18c3b4975bb76c8078a2c66f2fc0fa132d5
Christopher Elwell <elwell.christopher@gmail.com>
2016-04-28 13:59:46 -0700

    v1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject opt "1.0.1-SNAPSHOT"
+(defproject opt "1.0.2-SNAPSHOT"
   :description "Purple optimization library."
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.1.0"]
