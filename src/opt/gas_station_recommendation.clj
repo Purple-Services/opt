fdf4b3699a66736738f0b3c6ebc66ad1601f3eef
Christopher Elwell <elwell.christopher@gmail.com>
2016-06-02 14:30:49 -0700

    use opt in update-local...

--- src/opt/gas_station_recommendation.clj
+++ src/opt/gas_station_recommendation.clj
@@ -2,14 +2,14 @@
   (:require [opt.helpers.gas-station-recommendation-helpers :as helpers]))
 
 (defn update-local-stations-data
-  "Update the local stations.json file used to comptute the suggestions.
+  "Update the local stations.json file used to compute the suggestions.
 
   The argument 'opt' is an optional dictionary that accepts the following keys:
   <:extra_stations_file>: a file of extra stations (in json format). (optional)
 
   "
   [opt]
-  (helpers/update-local-stations-file))
+  (helpers/update-local-stations-file opt))
 
 (defn compute-suggestions
   "Take the lat/lng of source and destination (optional) and returns a list of
@@ -22,9 +22,11 @@
    Sample output: [{:station {...} :total-driving-time 2424},
                    {:station {...} :total-driving-time 4242}]"
   ([src-lat src-lng opt]
-    (helpers/suggest-gas-stations-near-with-score-price-based src-lng src-lat opt))
+   (helpers/suggest-gas-stations-near-with-score-price-based src-lng src-lat opt))
   ([src-lat src-lng dst-lat dst-lng opt]
-    (helpers/suggest-gas-stations-near-with-score-price-based src-lng src-lat dst-lng dst-lat opt)))
+   (helpers/suggest-gas-stations-near-with-score-price-based src-lng src-lat dst-lng dst-lat opt)))
 
-
-
+;; Call every Sunday
+;; (update-local-stations-data {:extra_stations_file "extra.json"})
+;; (update-local-stations-data {})
+;; (compute-suggestions 33.997034 -118.428448 {})
