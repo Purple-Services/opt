35ebba5acd6cd0e62491027ae89e497b1b31231f
Christopher Elwell <elwell.christopher@gmail.com>
2016-12-08 17:38:15 -0800

    keep profile config in ~/.lein/profiles.clj instead

--- project.clj
+++ project.clj
@@ -7,4 +7,17 @@
                  [com.twilio.sdk/twilio-java-sdk "4.2.0"]
                  [common "2.0.2-SNAPSHOT"]
                  [org.clojure/data.json "0.2.6"]]
-  :java-source-paths ["src/java"])
+  :java-source-paths ["src/java"]
+  :profiles {:shared [{:dependencies
+                       [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]
+                        [org.seleniumhq.selenium/selenium-java "2.47.1"]
+                        [clj-webdriver "0.7.2"]
+                        [ring "1.5.0"]
+                        [pjstadig/humane-test-output "0.6.0"]]
+                       :injections
+                       [(require 'pjstadig.humane-test-output)
+                        (pjstadig.humane-test-output/activate!)]}]
+             :local [:shared :profiles/local]
+             :dev [:shared :profiles/dev]
+             :prod [:shared :profiles/prod]})
